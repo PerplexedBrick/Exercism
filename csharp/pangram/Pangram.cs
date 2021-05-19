4f3bc6755f622ac2f78c4bb9384af577353105fe
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-20 00:08:50 +0300

    Finished Additional for 1st Task called Pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        if (input.Length > 25)
+        {
+            foreach (char c in alphabet)
+            {
+                bool IsLetterPresent = false;
+
+                foreach(char c1 in input.ToLower())
+                {
+                    if (c == c1)
+                    {
+                        IsLetterPresent = true;
+                    } 
+                    Console.WriteLine($"{c} == {c1}");
+                }
+
+                if (IsLetterPresent == false) return false;
+            }
+
+
+            return true;
+        }
+        return false;        
+    }
+
+}
