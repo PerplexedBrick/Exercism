deaddfd6728f3e3402f51fb1d50ba2b5082513d9
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-21 00:05:50 +0300

    Finished Side Exercise Isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string input)
+    {
+        string word = input.ToLower();
+        if (word.Length < 2) return true;
+        if (word.Length > 1)
+        {
+            for (int i=0; i < word.Length-1; i++)
+            {
+                if (IsLetter(word[i]))
+                {
+                    for(int j = i+1; j < word.Length; j++)
+                    {
+                        if (word[i] == word[j]) return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static bool IsLetter(char Letter)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        foreach (char c in alphabet)
+        {
+            if (Letter == c) return true;
+        }
+        return false;
+    }
+}
