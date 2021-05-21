d888e68e68c6a2c161c9172c1dbfc900d36d29ae
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-21 17:39:09 +0300

    Done Acronym exercise. Extract acronym from occasionally stupid string

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        Regex rgx = new Regex("[^a-zA-Z -]");
+        string abbreviation = rgx.Replace(phrase, "");
+
+        rgx = new Regex("(\\B[a-zA-Z])|( )|(-)");
+        return rgx.Replace(abbreviation, "").ToUpper();
+    }
+}
