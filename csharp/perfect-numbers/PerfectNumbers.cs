10e27367bff36f48b8130955efbfd4883042652b
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-22 00:09:59 +0300

    Done side exercise Perfect Numbers. Had to classify a natural number to the categories of perfect, abundant or deficient. Did some googling how to find all those dividers.

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+
+        if (number == 1) return Classification.Deficient;
+
+        int DivisorsSum = 1;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                DivisorsSum += i;
+                if (number / i != i) DivisorsSum += number / i;
+            }
+        }
+
+        if (number  > DivisorsSum) return Classification.Deficient;
+        if (number  < DivisorsSum) return Classification.Abundant;
+        return Classification.Perfect;
+    }
+}
