11856fe766790f31cf75a9cb4f02979d30f234fa
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-21 21:49:13 +0300

    Done side task Grains. Count number of grains on chessboard, basically raise 2 to power of a given number

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64) throw new ArgumentOutOfRangeException("n");
+        return (ulong)Math.Pow(2, (n - 1));
+    }
+
+    public static ulong Total()
+    {
+        ulong total = 0;
+        for(int i = 0; i < 64; i++)
+        {
+            total = total + (ulong)Math.Pow(2, i);
+        }
+        return total;
+    }
+}
