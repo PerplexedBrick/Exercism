04cfeac31c9af936e099d25d486fafc5ec71b566
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-22 12:32:07 +0300

    Done simple string reversal task

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if (input == "") return "";
+        char[] reversedInput = input.ToCharArray();
+        Array.Reverse(reversedInput);
+        return new string(reversedInput);
+    }
+}
