43132d0a28d4b8fa73d9baffbea1013437ffb292
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-24 20:58:10 +0300

    Done Collatz conjecture exercise. At last introduction to algorithms. Had to think for a while.

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number, int counter = 0)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+
+        if (number > 1)
+        {
+            if (number % 2 == 0) { counter = Steps(number / 2); }
+
+            if (number % 2 != 0) { counter = Steps(number * 3 + 1); }
+
+            counter++;
+        }
+
+        return counter;
+    }
+
+}
