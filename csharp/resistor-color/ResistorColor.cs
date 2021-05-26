5a8e5d8db8832f05d23336e397781749b74b18b9
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-26 20:54:21 +0300

    Finished 5 main task 'Resistor Color'. Well, it actually takes 2 lines to do it.

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    enum ColorCodes
+    {
+        black,
+        brown,
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        violet,
+        grey,
+        white
+    }
+
+    public static int ColorCode(string color) => (int)Enum.Parse(typeof(ColorCodes), color);
+     
+    public static string[] Colors() => Enum.GetNames(typeof(ColorCodes));
+}
