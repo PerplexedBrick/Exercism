9feaadf145f7387abcb685cf56708bed1afc0772
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-26 23:34:12 +0300

    Finished 'Sum of Multiples'. Starting to introduce myself to LINQ

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        List<int> multipliers = new List<int>();
+
+        foreach (int m in multiples)
+        {
+            if (m < 1 || m >= max ) continue;
+            multipliers.AddRange(Enumerable.Range(m, max - 1).Where(n => n < max).Where(n => n % m == 0));
+        }
+
+        return multipliers.Distinct().Sum();
+    }
+ 
+}
