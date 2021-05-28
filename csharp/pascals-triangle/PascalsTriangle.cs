b6d6c06aadd9eedd5abdda572d564f9e1565bd76
PerplexedBrick <perplexedbrick@gmail.com>
2021-05-28 23:21:01 +0300

    Done side task 'Pascal's Triangle'. I can clean up code but i'm tired already

---
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        if (rows == 0) return new List<int[]>();        
+
+        int[] row = new int[rows];
+
+        row[0] = 1;
+        row[row.Length - 1] = 1;
+
+        if( rows > 1)
+        {
+            IEnumerable < IEnumerable<int> > previousRows = Calculate(rows - 1);
+            int[] previousRow = (int[])previousRows.Last();
+
+            if( rows > 2)
+            {
+                for (int j = 1; j < rows-1; j++)
+                {
+                    row[j] = previousRow[j - 1] + previousRow[j];
+                }
+            }
+
+            return previousRows.Append(row);
+        }
+
+        var x = new List<int[]>();
+        x.Add(row);
+
+        return x;        
+    }
+
+}
