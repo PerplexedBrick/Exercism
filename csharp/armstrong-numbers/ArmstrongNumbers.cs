d3ec0f00f3184df6fa7efec72deb181952615e9d
PerplexedBrick <perplexedbrick@gmail.com>
2021-06-09 19:58:01 +0300

    Done Armstrng Numbers side task. Easy one again

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        if (number >= 0 && 9 >= number) return true;
+
+        var digits = NumbersIn(number);
+
+        int sum = (int)digits.Sum(x => Math.Pow(x, digits.Length));
+
+        return sum == number;
+    }
+
+    public static int[] NumbersIn(int value)
+    {
+        var numbers = new Stack<int>();
+
+        for (; value > 0; value /= 10)
+            numbers.Push(value % 10);
+
+        return numbers.ToArray();
+    }
+
+}
