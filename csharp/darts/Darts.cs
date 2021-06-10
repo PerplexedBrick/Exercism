640645bc847240b7beddd58a34042254625a1d3e
PerplexedBrick <perplexedbrick@gmail.com>
2021-06-10 23:17:04 +0300

    Done Darts exercise. It's very easy. Just calculate vector length

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double vectorLength = Math.Abs(Math.Sqrt(x * x + y * y));
+
+        if (vectorLength <= 1) return 10;
+        if (vectorLength <= 5) return 5;
+        if (vectorLength <= 10) return 1;
+        return 0;
+    }
+}
