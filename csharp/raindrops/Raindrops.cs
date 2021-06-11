393834596e8a15cb7cd25d027306f4abad4935f5
PerplexedBrick <perplexedbrick@gmail.com>
2021-06-11 13:32:09 +0300

    Done Raindrops side-task. Easy

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string output = "";
+
+        if (number % 3 == 0) output += "Pling";
+        if (number % 5 == 0) output += "Plang";
+        if (number % 7 == 0) output += "Plong";
+
+        if (string.IsNullOrEmpty(output)) return number.ToString();
+        return output;
+    }
+}
