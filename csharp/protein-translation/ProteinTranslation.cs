16ce4a5dc67bfbab2197ce7c50241b19797333d2
PerplexedBrick <perplexedbrick@gmail.com>
2021-06-14 18:32:23 +0300

    Done Protein Translation side-task. Comparison of substrings to patterns.

---
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProteinTranslation
+{
+    public static string[] Proteins(string strand)
+    {
+
+        List<string> codons = new List<string>(); 
+
+        for(int i = 0; i < strand.Length; i += 3)
+        {
+            string codon = strand.Substring(i, 3);
+            if (codon == "UAA" || codon == "UAG" || codon == "UGA") break;
+            codons.Add(codon switch
+            {
+                "AUG" => "Methionine",
+                "UUU" => "Phenylalanine",
+                "UUC" => "Phenylalanine",
+                "UUA" => "Leucine",
+                "UUG" => "Leucine",
+                "UCU" => "Serine",
+                "UCC" => "Serine",
+                "UCA" => "Serine",
+                "UCG" => "Serine",
+                "UAU" => "Tyrosine",
+                "UAC" => "Tyrosine",
+                "UGU" => "Cysteine",
+                "UGC" => "Cysteine",
+                "UGG" => "Tryptophan",
+                _ => ""
+            });
+        }
+
+
+        return codons.ToArray();
+
+    }
+}
